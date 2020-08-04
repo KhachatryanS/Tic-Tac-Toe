937cb2057923568fea57af72cf14e052730de8da
Susanna Khachatryan <46651903+Susanna01@users.noreply.github.com>
2020-08-04 18:39:28 +0400

    Added comments and cleaned up code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         char[][] cells = new char[3][3];
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {                           //initializing all elements as empty cells
             for (int j = 0; j < 3; j++) {
                 cells[i][j] = ' ';
             }
         }
         print(cells);
-//        System.out.println(state(cells));
         int a;
         int b;
         boolean flag = true;
@@ -26,19 +26,20 @@ public class Main {
                 if (cells[3 - b][a - 1] != ' ') {
                     System.out.println("This cell is occupied! Choose another one!");
                 } else {
-                    cells[3 - b][a - 1] = 'X';
+                    cells[3 - b][a - 1] = flag ? 'X' : 'O';     //changing the player
+                    flag = !flag;
                     print(cells);
                     if (state(cells)) {
-                        break;
+                        break;                                  //if someone wins or there is a draw loop breaks
                     }
                 }
-            } catch (Exception e) {
+            } catch (InputMismatchException e) {                //catch an exception when input is not a number
                 System.out.println("You should enter only numbers!");
             }
         }
     }
 
-    public static void print(char[][] cells) {
+    public static void print(char[][] cells) {                  //function prints gaming table
         System.out.println("---------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
@@ -50,10 +51,10 @@ public class Main {
         System.out.println("---------");
     }
 
-    public static boolean state(char[][] cells) {
-        if (Wins(cells, 'X')) {
-            System.out.println("X wins");
-            return true;
+    public static boolean state(char[][] cells) {               //function prints winner's name if there is a winner
+        if (Wins(cells, 'X')) {                            //or prints "draw" if no one wins
+            System.out.println("X wins");                       //if the game isn't over yet it returns false
+            return true;                                        //otherwise returns true
         }
         if (Wins(cells, 'O')) {
             System.out.println("O wins");
@@ -66,7 +67,7 @@ public class Main {
         return false;
 
     }
-    public static boolean Wins(char[][] cells, char chr) {
+    public static boolean Wins(char[][] cells, char chr) {      //function returns if the given character wins
         for (int i = 0; i < 3; i++) {
             if (cells[0][i] == chr && cells[1][i] == chr && cells[2][i] == chr ||
                     cells[i][0] == chr && cells[i][1] == chr && cells[i][2] == chr) {
@@ -77,7 +78,7 @@ public class Main {
                 cells[0][2] == chr && cells[1][1] == chr && cells[2][0] == chr;
     }
 
-    public static boolean hasEmptyCells(char[][] cells) {
+    public static boolean hasEmptyCells(char[][] cells) {       //function returns if there are empty cells
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (cells[i][j] == ' '){
@@ -86,19 +87,5 @@ public class Main {
             }
         }
         return false;
-    }
-    public static boolean possibility(char[][] cells) {
-        int countX = 0;
-        int countO = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (cells[i][j] == 'X') {
-                    countX++;
-                } else if (cells[i][j] == 'O') {
-                    countO++;
-                }
-            }
-        }
-        return Math.abs(countX - countO) == 1 || Math.abs(countX - countO) == 0;
     }
 }
